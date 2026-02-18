2440a3a97a658a02769bcb190de0941a5542dc2b
Huanyingcraft21 <112355285+Huanyingcraft21@users.noreply.github.com>
2026-02-18 16:37:12 +0800

    Create config_sample.php

---
+++ config_sample.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'db' => ['host'=>'127.0.0.1', 'name'=>'authme', 'user'=>'root', 'pass'=>''],
+    'smtp' => ['host'=>'smtp.qq.com', 'port'=>465, 'user'=>'', 'pass'=>'', 'secure'=>'ssl', 'from_name'=>'流星MCS'],
+    'admin' => ['user'=>'admin', 'pass'=>'password123', 'email'=>''],
+    'site' => ['title'=>'流星MCS', 'ver'=>'1.6', 'bg'=>''],
+    'server' => ['ip'=>'', 'port'=>'25565'],
+    'rcon' => ['host'=>'127.0.0.1', 'port'=>25575, 'pass'=>''],
+    'rewards' => ['reg_cmd'=>'', 'daily_cmd'=>'']
+];
