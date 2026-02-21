023997b79de8c7894850380c6e317f642d212d10
Huanyingcraft21 <112355285+Huanyingcraft21@users.noreply.github.com>
2026-02-21 13:40:55 +0800

    Update config_sample.php

--- config_sample.php
+++ config_sample.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * Project: Meteor Nexus 配置文件蓝图
- * Version: 2.1.4
+ * Version: 2.1.5
  */
 return [
     'db' => ['host' => '127.0.0.1', 'name' => 'authme', 'user' => 'root', 'pass' => ''],
     'smtp' => ['host' => 'smtp.qq.com', 'port' => 465, 'user' => '', 'pass' => '', 'secure' => 'ssl', 'from_name' => '流星网'],
     'admin' => ['user' => 'admin', 'pass' => 'password123', 'email' => ''],
-    'site' => ['title' => 'Meteor Nexus', 'ver' => '2.1.1', 'bg' => ''],
+    'site' => ['title' => 'Meteor Nexus', 'ver' => '2.1.5', 'bg' => ''],
     'display' => ['ip' => '', 'port' => '25565'], 
     'servers' => [['name' => '默认节点', 'ip' => '127.0.0.1', 'port' => 25565, 'api_port' => 8080, 'api_key' => '']],
     'rewards' => ['reg_cmd' => '', 'daily_cmd' => '', 'sign_in_servers' => [0]],
